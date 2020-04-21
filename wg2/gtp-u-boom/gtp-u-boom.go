20be9f4653abb9e571127254b4be6917616ce65f
Bjørn Nordbø <bn@wgtwo.com>
2020-04-21 16:15:21 +0200

    Fix errors

--- wg2/gtp-u-boom/gtp-u-boom.go
+++ wg2/gtp-u-boom/gtp-u-boom.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"github.com/intel-go/nff-go/flow"
 	"github.com/intel-go/nff-go/packet"
 	"github.com/intel-go/nff-go/types"
 )
 
-const src = types.BytesToIPv4(1, 2, 3, 4)
-const dst = types.BytesToIPv4(48, 8, 8, 5)
+var SRC = types.BytesToIPv4(1, 2, 3, 4)
+var DST = types.BytesToIPv4(48, 8, 8, 5)
 
 func main() {
 	flow.SystemInit(&flow.Config{CPUList: "0-9"})
 
-	mainFlow := flow.SetFastGenerator(genICMP, nil)
+	mainFlow := flow.SetGenerator(genICMP, nil)
 	flow.SetHandlerDrop(mainFlow, encap, nil)
 
 	flow.SystemStart()
 }
 
 func encap(p *packet.Packet, c flow.UserContext) bool {
-	if current.EncapsulateIPv4GTP("TODO get from flags") == false {
-
+	if p.EncapsulateIPv4GTP("TODO get from flags") == false {
+		log.Println("Error encapsulating GTP-U packet")
+		return false
 	}
 
+	return true
 }
 
 func genICMP(p *packet.Packet, c *flow.UserContext) {
@@ -30,8 +34,8 @@ func genICMP(p *packet.Packet, c *flow.UserContext) {
 	packet.InitEmptyIPv4UDPPacket(p, payload)
 	ipv4 := p.GetIPv4NoCheck()
 	udp := p.GetUDPNoCheck()
-	ipv4.SrcAddr = src
-	ipv4.DstAddr = dst
+	ipv4.SrcAddr = SRC
+	ipv4.DstAddr = DST
 	udp.SrcPort = packet.SwapBytesUint16(1234)
 	udp.DstPort = packet.SwapBytesUint16(2152)
 }
