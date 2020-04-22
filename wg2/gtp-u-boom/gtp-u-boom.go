d4d531ef8dccba36cce215496c0f5e2da7dc43d0
Etienne Glossi <etienne@wgtwo.com>
2020-04-22 19:27:23 +0200

    Fixed script to send packet

--- wg2/gtp-u-boom/gtp-u-boom.go
+++ wg2/gtp-u-boom/gtp-u-boom.go
@@ -42,11 +42,34 @@ func main() {
 		return encap(p, c, srcAddr, dstAddr, pkID)
 	}
 
-	flow.SystemInit(&flow.Config{CPUList: "0-9"})
+	config := flow.Config{
+		// Is required for KNI
+		NeedKNI: false,
+		CPUList: "0-3",
+		SendCPUCoresPerPort: 2,
+		TXQueuesNumberPerPort: 8,
+	}
+
+	flow.CheckFatal(flow.SystemInit(&config))
+	//kni, err := flow.CreateKniDevice(uint16(*kniport), "kni_eth1")
+	//flow.CheckFatal(err)
+	//inputFlow, err := flow.SetReceiver(uint16(0))
+	//flow.CheckFatal(err)
+	//toKNIFlow, err := flow.SetSeparator(inputFlow, arpSeparator, nil)
+	//flow.CheckFatal(err)
+	//fromKNIFlow, err := flow.SetSenderReceiverKNI(toKNIFlow, kni, true)
+	//flow.CheckFatal(err)
+	//outputFlow, err := flow.SetMerger(inputFlow, fromKNIFlow)
+	//flow.CheckFatal(err)
+	//flow.CheckFatal(flow.SetSender(outputFlow, uint16(0)))
 
 	mainFlow := flow.SetGenerator(genFn, nil)
 	flow.SetHandlerDrop(mainFlow, encapFn, nil)
-	flow.SetStopper(mainFlow)
+	//flow.SetSenderFile(mainFlow, "/tmp/gtp-u.pcap")
+	// Send all generated packets to the output port
+	flow.CheckFatal(flow.SetSender(mainFlow, 0))
+
+	//flow.SetStopper(mainFlow)
 
 	flow.SystemStart()
 }
@@ -63,6 +86,9 @@ func encap(
 	}
 
 	p.ParseL3()
+	p.Ether.SAddr = [6]uint8{0x06, 0xc5, 0x79, 0x20, 0xd0, 0x60}
+	p.Ether.DAddr = [6]uint8{0x06, 0x9a, 0x4b, 0x5a, 0x34, 0xa0}
+	p.Ether.EtherType = types.SwapIPV4Number
 	ipv4 := p.GetIPv4NoCheck()
 	length := p.GetPacketLen()
 
@@ -78,17 +104,21 @@ func encap(
 	ipv4.DstAddr = dstAddr
 	ipv4.HdrChecksum = packet.SwapBytesUint16(packet.CalculateIPv4Checksum(ipv4))
 
+	p.ParseL4ForIPv4()
 	udp := p.GetUDPNoCheck()
-	udp.SrcPort = packet.SwapUDPPortGTPU
-	udp.DstPort = packet.SwapUDPPortGTPU
-	udp.DgramLen = uint16(length - types.EtherLen - types.IPv4MinLen)
+
+	udp.SrcPort = packet.SwapBytesUint16(2152)
+	udp.DstPort = packet.SwapBytesUint16(2152)
+	udp.DgramLen = packet.SwapBytesUint16(uint16(length - types.EtherLen - types.IPv4MinLen))
 	udp.DgramCksum = 0
 
 	return true
 }
 
 func genICMP(p *packet.Packet, c flow.UserContext, data *string) {
-	payload, _ := hex.DecodeString(*data)
+	// adding fake Ethernet header since encapsulation will truncate it without checking if it exist
+	ether := "0000000000000000000000000000"
+	payload, _ := hex.DecodeString(ether + *data)
 	packet.GeneratePacketFromByte(p, payload)
 }
 
@@ -99,5 +129,41 @@ func stringToIPv4(addr string) (types.IPv4Address, error) {
 	}
 	i := ip.To4()
 
-	return types.BytesToIPv4(i[0], i[1], i[2], i[2]), nil
+	return types.BytesToIPv4(i[0], i[1], i[2], i[3]), nil
 }
+
+var np = 0
+
+func dump(currentPacket *packet.Packet, context flow.UserContext) {
+	if np < 9 /*dump first three packets */ {
+		fmt.Printf("%v", currentPacket.Ether)
+		currentPacket.ParseL3()
+		ipv4 := currentPacket.GetIPv4()
+		if ipv4 != nil {
+			fmt.Printf("%v", ipv4)
+			tcp, udp, _ := currentPacket.ParseAllKnownL4ForIPv4()
+			if tcp != nil {
+				fmt.Printf("%v", tcp)
+			} else if udp != nil {
+				fmt.Printf("%v", udp)
+				gtp := currentPacket.GTPIPv4FastParsing()
+				fmt.Printf("%v", gtp)
+			} else {
+				println("ERROR L 1")
+			}
+		} else {
+			println("ERROR L 0")
+		}
+		fmt.Println("----------------------------------------------------------")
+		np++
+	}
+}
+
+func arpSeparator(p *packet.Packet, c flow.UserContext) bool {
+	p.ParseL3()
+	if p.GetARP() != nil {
+		return false
+	}
+	return true
+}
+
